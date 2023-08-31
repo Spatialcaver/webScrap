f153b2f10fb5cefb90ccf99c7d7845d86aa04ef0
Spatialcaver <101186168+Spatialcaver@users.noreply.github.com>
2023-08-30 21:46:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import csv
-import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 
-url = "https://www.magazineluiza.com.br/"
+url = "https://www.amazon.com.br/s?k=livros&__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_1"
 
 response = requests.get(url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
 
-    produtos = soup.find_all('div', class_='pbox col-xs-12 col-sm-6 col-md-3 col-lg-1-5')
+    produtos = soup.find_all('div', class_='a-section a-spacing-base')
 
     file = open('export_data.csv', 'w', newline='')
     writer = csv.writer(file)
-    headers = ['produto ',  'preço']
+    headers = ['produto ' , 'preco']
     writer.writerow(headers)
 
     for headline in produtos:
-        produto = headline.find('div', class_='commerce_columns_item_caption').text.strip()
-        preco = headline.find('div', class_='sc-fqkvVR dzACBc sc-bfhvDw eCuCbE').text.strip()
+        produto = headline.find('span', class_='a-size-base-plus a-color-base a-text-normal').text.strip()
+
+        preco = headline.find('span', class_='a-offscreen').text.strip()
         
         row = [produto, preco]
         writer.writerow(row)
 
-        print('produto: ', produto, ' |', preco) 
+        print('produto: ', produto , '|', preco) 
     file.close()
     print("deu tudo certo meu chapa!")
 
